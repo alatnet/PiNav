ba6968db7dbec57fc43da446b44e1e9b5baf4c83
alatnet <alatnet1@gmail.com>
2018-12-09 23:58:46 -0500

    first commit

---
+++ install_pinav.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#setup usb gadget functionality
+#echo "dtoverlay=dwc2" > sudo tee -a /boot/config.txt
+#echo "dwc2" | sudo tee -a /etc/modules
+#echo "libcomposite" | sudo tee -a /etc/modules
+
+#enable console on serial
+#systemctl enable getty@ttyGS0.service
+
+#copy and enable the pinav usb gadget service
+cp /boot/pinav/pinav_usb /usr/bin/pinav_usb
+chmod +x /usr/bin/pinav_usb
+cp /boot/pinav/pinav_usb.service /etc/systemd/system/pinav_usb.service
+systemctl enable pinav_usb
+
+#copy the bridge daemon
+cp /boot/pinav/99-pinav_bridge_daemon.rules /etc/udev/rules.d
+cp /boot/pinav/pinav_bridge_daemon@.service /etc/systemd/system
+cp /boot/pinav/pinav_bridge_daemon /usr/bin
+chmod +x /usr/bin/pinav_bridge_daemon
+
+#copy the navpair program
+cp /boot/pinav/navpair /usr/bin
+chmod +x /usr/bin/navpair
---
+++ uninstall_pinav.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#disable console on serial
+#systemctl disable getty@ttyGS0.service
+
+#disable and remove the pinav usb gadget service
+systemctl disable pinav_usb
+rm /etc/systemd/system/pinav_usb.service
+rm /usr/bin/pinav_usb
+
+#remove the bridge daemon
+rm /boot/pinav/99-pinav_bridge_daemon.rules /etc/udev/rules.d
+rm /boot/pinav/pinav_bridge_daemon@.service /etc/systemd/system
+rm /usr/bin/pinav_bridge_daemon
+
+#remove the navpair program
+rm /usr/bin/navpair
