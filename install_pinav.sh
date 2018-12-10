5828607c1746b4a5cec4504bfcd7c60a2e320072
alatnet <alatnet1@gmail.com>
2018-12-10 03:41:20 -0500

    bugfixes

--- install_pinav.sh
+++ install_pinav.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #setup usb gadget functionality
-#echo "dtoverlay=dwc2" > sudo tee -a /boot/config.txt
+#echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 #echo "dwc2" | sudo tee -a /etc/modules
 #echo "libcomposite" | sudo tee -a /etc/modules
 
